fce35212df380bbc10b627ee78444118e67837ae
Kartik <kartiksibal@gmail.com>
2016-08-17 23:59:50 +0530

    Compare the triplets.
    Status:

---
+++ Compare the triplets.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+int main(int argc, char *argv[]) {
+	
+	
+	int a [10];
+	int b [10];
+	
+	for (int i = 0 ; i < 10; i++)
+	{
+		cin >> a[i];
+	}
+	
+	
+	for (int j = 0 ; j < 10; j++)
+	{
+		cin >> b[j];
+	}
+
+	for (int l = 0 ; l < 10; l++)
+	{
+		if (a[i] >
+	}
+
+
+
+
+
+
+}
